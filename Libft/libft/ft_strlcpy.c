e8e0f25b3f4a2744a8aadd90edd00253ecf61751
Ysilla Eschallots <yeschall@et-m1.kzn.21-school.ru>
2020-11-12 21:29:55 +0300

    my lib added

---
+++ Libft/libft/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeschall <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/05 21:35:15 by yeschall          #+#    #+#             */
+/*   Updated: 2020/11/05 21:35:16 by yeschall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (dst == NULL || src == NULL)
+		return (0);
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (src[i] && (dstsize - i - 1) > 0)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
