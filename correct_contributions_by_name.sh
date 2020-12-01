39448bd9bd1ff747b8e0891a0354d3071ff23010
farscince <farscince@gmail.com>
2020-12-01 19:47:56 +0300

    script correcting contributions by name created

---
+++ correct_contributions_by_name.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# see instruction on:
+# https://stackoverflow.com/questions/15289768/github-commits-arent-recorded-in-the-your-contributions-calendar/19183663
+# msg from user GinTonic
+
+git filter-branch --env-filter '
+
+OLD_NAME="Ysilla Eschallots"
+OLD_EMAIL="yeschall@student.21-school.ru"
+CORRECT_NAME="farscince"
+CORRECT_EMAIL="farscince@gmail.com"
+
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
