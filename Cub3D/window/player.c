fcc31c5ec338f31291a1032f147f202f013e382f
farscince <farscince@gmail.com>
2021-02-14 18:23:59 +0300

    fisheye effect was corrected

--- Cub3D/window/player.c
+++ Cub3D/window/player.c
@@ -7,31 +7,38 @@
 ** cross of the ray and grid
 */
 
-static double		count_ray_len(t_set *set, t_pix *map, t_fpix *ray_dir)
+static double		count_ray_len(t_set *set, t_fpix *ray_dir)
 {
 	t_fpix			cross;
 	t_fpix			dist;
+	t_pix			map;
 
-//	DEBUG printf("Player is on\n\t[%f, %f]\nsin(%f) = %f\ncos(%f) = %f\n", set->player.pos.y, set->player.pos.x, set->player.angle, sin(set->player.angle), set->player.angle, cos(set->player.angle));
-	cross.x = (ray_dir->x < 0) ?
-			map->x + set->player.step.x + 1 : map->x + set->player.step.x;
-	cross.y = (ray_dir->y < 0) ?
-			map->y + set->player.step.y + 1 : map->y + set->player.step.y;
-	dist.x = (ray_dir->x == 0) ? dist.x + 1 :
-			fabs((cross.x - set->player.pos.x) / (ray_dir->x / v_len(*ray_dir)));
-	dist.y = (ray_dir->y == 0) ? dist.y + 1 :
-			fabs((cross.y - set->player.pos.y) / (ray_dir->y / v_len(*ray_dir)));
-	if (dist.x < dist.y)
+	set->player.step.x = (ray_dir->x < 0) ? -1 : 1;
+	set->player.step.y = (ray_dir->y < 0) ? -1 : 1;
+	map.x = (int)set->player.pos.x;
+	map.y = (int)set->player.pos.y;
+	while (set->map.c_map[map.y][map.x] != '1')
 	{
-		cross.y = set->player.pos.y + dist.x * ray_dir->y / v_len(*ray_dir);
-		map->x += set->player.step.x;
+		cross.x = (ray_dir->x < 0) ?
+				map.x + set->player.step.x + 1 : map.x + set->player.step.x;
+		cross.y = (ray_dir->y < 0) ?
+				map.y + set->player.step.y + 1 : map.y + set->player.step.y;
+		dist.x = (ray_dir->x == 0) ? dist.x + 1 :
+				fabs((cross.x - set->player.pos.x) / (ray_dir->x / v_len(*ray_dir)));
+		dist.y = (ray_dir->y == 0) ? dist.y + 1 :
+				fabs((cross.y - set->player.pos.y) / (ray_dir->y / v_len(*ray_dir)));
+		if (dist.x < dist.y)
+		{
+			cross.y = set->player.pos.y + dist.x * ray_dir->y / v_len(*ray_dir);
+			map.x += set->player.step.x;
+		}
+		else
+		{
+			cross.x = set->player.pos.x + dist.y * ray_dir->x / v_len(*ray_dir);
+			map.y += set->player.step.y;
+		}
+		my_mlx_pixel_put(set, cross.x * SCALE, cross.y * SCALE, 0xFF0000);
 	}
-	else
-	{
-		cross.x = set->player.pos.x + dist.y * ray_dir->x / v_len(*ray_dir);
-		map->y += set->player.step.y;
-	}
-	my_mlx_pixel_put(set, cross.x * SCALE, cross.y * SCALE, 0xFF0000);
 	return (dist.x < dist.y ? dist.x : dist.y);
 }
 
@@ -54,7 +61,7 @@ void				drop_rays(t_set *set)
 {
 	int				x;
 	double			dist;
-	t_pix			map;
+
 	double			cameraX;
 	t_fpix			ray_dir;
 	double perpDist;
@@ -63,15 +70,10 @@ void				drop_rays(t_set *set)
 	x = 0;
 	while (x < set->win.res1)
 	{
-		map.x = (int)set->player.pos.x;
-		map.y = (int)set->player.pos.y;
 		cameraX = 2 * x / (double)set->win.res1 - 1;
 		ray_dir.x = set->player.dir.x + cameraX * set->player.plane.x;
 		ray_dir.y = set->player.dir.y + cameraX * set->player.plane.y;
-		set->player.step.x = (ray_dir.x < 0) ? -1 : 1;
-		set->player.step.y = (ray_dir.y < 0) ? -1 : 1;
-		while (set->map.c_map[map.y][map.x] != '1')
-			dist = count_ray_len(set, &map, &ray_dir);
+		dist = count_ray_len(set, &ray_dir);
 		perpDist = dist * v_mult(ray_dir, set->player.dir)/v_len(set->player.dir) / v_len(ray_dir);
 		DEBUG printf("dist: [%f]\n", perpDist);
 		draw_line(set, perpDist, x);
