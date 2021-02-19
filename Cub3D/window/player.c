77a6fd09541ffa175c5eb80649bbecc03187bfd1
farscince <farscince@gmail.com>
2021-02-19 14:42:40 +0300

    draw_strip is refactored

--- Cub3D/window/player.c
+++ Cub3D/window/player.c
@@ -19,32 +19,27 @@ unsigned int	get_color(t_img *img, int x, int y)
 void				draw_strip(t_set *set, double dist, int x, t_fpix *cross, int side)
 {
 	double			h;
-	int				y0;
-	int				y1;
+	t_pix			strip;
+	t_pix			wall;
 	double			k;
-	double scale;
+	double			y;
 
-	int wallX;
-	int wallY;
 	k = set->player.hor / HOR;
 	h = (double)set->win.img1.res.y / dist * k;
 
-	scale = (double)h / (double)set->win.skins[side].res.y;
-
-	double y;
-	y = (double)set->win.img1.res.y / set->player.hor - h / 2;
-	y = y <= 0 ? 1 : y;
-	y1 = set->win.img1.res.y / set->player.hor + h / 2;
-	y1 = y1 >= set->win.img1.res.y ? set->win.img1.res.y - 1 : y1;
-	y0 = y;
+	strip.x = (int)((double)set->win.img1.res.y / set->player.hor - h / 2);
+	strip.x = strip.x <= 0 ? 1 : strip.x;
+	strip.y = set->win.img1.res.y / set->player.hor + h / 2;
+	strip.y = strip.y >= set->win.img1.res.y ? set->win.img1.res.y - 1 : strip.y;
+	y = strip.x;
 	if (side % 2) // != 0
-		wallX = (int)((set->win.skins[side].res.y - 1) * fmod(cross->x, 1.0));
+		wall.x = (int)((set->win.skins[side].res.y - 1) * fmod(cross->x, 1.0));
 	else
-		wallX = (int)((set->win.skins[side].res.y - 1) * (1 - fmod(cross->y, 1.0)));
-	while(y < y1)
+		wall.x = (int)((set->win.skins[side].res.y - 1) * (1 - fmod(cross->y, 1.0)));
+	while(y < strip.y)
 	{
-		wallY = (int)((y - (double)y0) / scale);
-		my_mlx_pixel_put(set, x, (int)y, get_color(set->win.skins[side].img, wallX, wallY));
+		wall.y = (int)((y - (double)strip.x) * (double)set->win.skins[side].res.y / (double)h);
+		my_mlx_pixel_put(set, x, (int)y, get_color(set->win.skins[side].img, wall.x, wall.y));
 		y++;
 	}
 }
