1b5d2b9954ea79815dabccebb586a0284bc30e58
farscince <farscince@gmail.com>
2021-03-30 21:40:27 +0300

    docker is almost working

--- Docker/LEMP_debian/start.sh
+++ Docker/LEMP_debian/start.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 service nginx start
 service php7.3-fpm start
@@ -10,22 +10,26 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTIO
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
 echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
 
-cp -fr /wordpress /var/www/html/
-cp -fr /phpmyadmin /var/www/html/
+
+
+ln -s /wordpress /var/www/html/wordpress
+ln -s /phpmyadmin /var/www/html/phpmyadmin
 
 
 echo $1
 
 
 
-if [ $1 = "off" ];  then
-  echo "here"
+if [[ $1 == "off" ]];  then
   sed -i "17 s/autoindex on;/autoindex off;/" /etc/nginx/sites-available/nginx.config
+  echo "autoindex is off..."
+else
+  echo "autoindex is on..."
 fi
 #if [ $1 = AUTOINDEX=on ];  then
 #   	sed -i "17 s/autoindex off;/autoindex on;/" /etc/nginx/sites-available/nginx.config
 #fi
 
+service nginx restart
 # sleep 100000000000
-
 bash
