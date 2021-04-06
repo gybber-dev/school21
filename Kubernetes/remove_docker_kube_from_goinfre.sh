0da4ceaf178e2bbbc3bd56a2ec12b052651c52d8
farscince <farscince@gmail.com>
2021-04-06 12:13:10 +0300

    Scripts for kuber are added

--- Kubernetes/remove_docker_kube_from_goinfre.sh
+++ Kubernetes/remove_docker_kube_from_goinfre.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# sh docker_setup.sh
 # from https://gist.github.com/gcamerli/b8cf836f8627cb285f33086db10096a2
 GOINFRE="/goinfre/$USER";
 echo $GOINFRE
@@ -13,5 +13,7 @@ rm -fr $GOINFRE/Containers
 rm -fr $GOINFRE/Caches/
 rm -fr ~/Library/Caches/com.docker.docker
 mkdir -p ~/Library/Caches/com.docker.docker
+rm -fr $GOINFRE/.minikube
+rm -fr ~/.minikube
+mkdir -p ~/.minikube
 echo "goinfre is clear"
-
