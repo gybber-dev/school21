0b05a717c2920a24cfb27bdbecc454c7d6bba5a6
farscince <farscince@gmail.com>
2021-04-08 21:21:39 +0300

    colour msgs was added

---
+++ Kubernetes/server/color.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#https://habr.com/ru/post/119436/
+
+BOLD='\033[1m'
+NORMAL='\033[0m'
+YELLOW='\033[33m'
+MY_MSG='\033[1;33m'
+
+
+echo -e "${MY_MSG}Жирный Текст${NORMAL}"
+echo "simple text"
--- Kubernetes/server/setup.sh
+++ Kubernetes/server/setup.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+MY_MSG='\033[1;33m'
+NORMAL='\033[0m'
+# Sample:
+# echo -e "${MY_MSG}sample${NORMAL}"
+
+
+
 NGINX_IMG="nginx_img";
 
-
+# start minikube
+echo -e "${MY_MSG}MINIKUBE STARTING...${NORMAL}"
 minikube start --vm-driver=virtualbox
 eval $(minikube -p minikube docker-env)
 
+# create images:
+echo -e "${MY_MSG}CREATING images${NORMAL}"
 docker build . -t $NGINX_IMG
+
+# set configs for Kubernetes
+echo -e "${MY_MSG}MetalLB enable...${NORMAL}"
+minikube addons enable metallb
+
+
+# apply configs:
+kubectl apply -f nginx.yml
+echo -e "${MY_MSG}Waiting for pods' starting...${NORMAL}"
+sleep 2s
+kubectl get pods
 
 #optionally:
 sleep 5s
