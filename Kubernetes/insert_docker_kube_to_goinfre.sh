be6d30966ff8f8b7fd22011f2f94656a84cdc40e
farscince <farscince@gmail.com>
2021-04-08 16:10:33 +0300

    .minikube directory creation was corrected

--- Kubernetes/insert_docker_kube_to_goinfre.sh
+++ Kubernetes/insert_docker_kube_to_goinfre.sh
@@ -3,8 +3,8 @@
 # from https://gist.github.com/gcamerli/b8cf836f8627cb285f33086db10096a2
 GOINFRE="/goinfre/$USER";
 echo $GOINFRE
-mkdir -p $GOINFRE/.minikube
 rm -fr ~/.minikube/
+mkdir -p $GOINFRE/.minikube
 ln -sf $GOINFRE/.minikube ~/.minikube
 mkdir -p $GOINFRE/Caches
 mkdir -p $GOINFRE/Caches/com.docker.docker
