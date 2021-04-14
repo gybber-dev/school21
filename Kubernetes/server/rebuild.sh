f0c7003f53295856d6ac9c6b43084e5b33518ea0
farscince <farscince@gmail.com>
2021-04-14 20:52:52 +0300

    foo

--- Kubernetes/server/rebuild.sh
+++ Kubernetes/server/rebuild.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 NGINX_IMG="nginx_img";
+WORDPRESS_IMG="wordpress_img"
 
 eval $(minikube -p minikube docker-env)
-docker build . -t $NGINX_IMG
+
+#docker build . -t $NGINX_IMG
+docker build srcs/wordpress/. -t $WORDPRESS_IMG
+
 echo "Deleting nodes:"
 kubectl get all
-kubectl delete deploy --all
-kubectl delete svc --all
+kubectl delete deploy wordpress-deploy
+kubectl delete svc wordpress-svc
 #echo "Create nginx pod:"
 #kubectl create -f nginx.yml
+
 kubectl apply -f nginx.yml
+kubectl apply -f srcs/wordpress/srcs/wordpress.yaml
+
 echo "Waiting for pods' starting..."
 sleep 2s
 kubectl get all
---
+++ Kubernetes/server/srcs/nginx_2/srcs/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "NGINX 2 is starting..."
+
+#sh
+nginx -g 'daemon off;'
