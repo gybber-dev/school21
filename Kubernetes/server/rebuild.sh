1b027293edd64d497e3ac4de583839aebf7ddded
farscince <farscince@gmail.com>
2021-04-20 19:21:32 +0300

    mysql with nginx. Todo: delete nginx

--- Kubernetes/server/rebuild.sh
+++ Kubernetes/server/rebuild.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-NGINX_IMG="nginx_img";
-WORDPRESS_IMG="wordpress_img"
+DEPLOY_NAME="sql-deploy";
+SERVICE_NAME="sql-svc"
 
 eval $(minikube docker-env)
 
 #docker build . -t $NGINX_IMG
-docker build srcs/wordpress/. -t $WORDPRESS_IMG
+#docker build srcs/wordpress/. -t $WORDPRESS_IMG
 
 echo "Deleting nodes:"
 kubectl get all
-kubectl delete deploy wordpress
-kubectl delete svc wordpress-svc
+kubectl delete deploy ${DEPLOY_NAME}
+kubectl delete svc ${SERVICE_NAME}
 #echo "Create nginx pod:"
 #kubectl create -f nginx.yml
 
-kubectl apply -f nginx.yml
-kubectl apply -f srcs/wordpress/srcs/wordpress.yaml
+#kubectl apply -f nginx.yml
+#kubectl apply -f srcs/wordpress/srcs/wordpress.yaml
 
 echo "Waiting for pods' starting..."
 sleep 2s
