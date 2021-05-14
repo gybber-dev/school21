79ba9e8379dadade99691090a73e461e7c1c7031
farscince <farscince@gmail.com>
2021-05-14 14:58:17 +0300

    ftps server was run

--- Kubernetes/server/srcs/ftps/srcs/start.sh
+++ Kubernetes/server/srcs/ftps/srcs/start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 echo "ftps is starting..."
+adduser -D alex
+echo "alex:alex" | chpasswd
+mkdir /home/alex/ftp
+chown nobody:nogroup /home/alex/ftp
+chmod a-w /home/alex/ftp
+mkdir /home/alex/ftp/files
+chown alex:alex /home/alex/ftp/files
+echo "vsftpd sample file" > /home/alex/ftp/files/sample.txt
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+echo "running ftp server..."
+vsftpd etc/vsftpd/vsftpd.conf
